f933360e79343634150f80deb5cc221935fcdbf0
Glenn Ou <2738328919@qq.com>
2022-04-05 10:35:12 +0800

    login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -28,14 +28,21 @@ try{
 
 router.post('/login', async (req, res) =>{
   try{
+
+    // check the email 
      const user = await User.findOne({email: req.body.email});
+    //  no email in the database, then return a error
      !user && res.status(404).json("User not found!");
 
+    //  hash the password
      const validPassword = await bcrypt.compare(req.body.password,user.password);
-    
+    // password does not match, then return error
      !validPassword && res.status(404).json("password not found!");
 
+    //  all match in the database, then send out the user
      res.status(200).json(user);
+
+
 
    }catch (err){
     res.status(500).json(err);
